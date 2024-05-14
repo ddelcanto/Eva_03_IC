817d25de4671b472566a19349e42d12ab91b7e65
Daniel del Canto <daniel.delcanto.arce@outlook.com>
2024-05-13 22:50:01 -0400

    6

--- src/main/java/com/ddelcanto/eva_03/Inicio.java
+++ src/main/java/com/ddelcanto/eva_03/Inicio.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
 package com.ddelcanto.eva_03;
 
 import java.util.UUID;
@@ -10,13 +10,16 @@ import java.util.UUID;
  *
  * @author danieldelcanto
  */
-public class Eva_03 {
+public class Inicio {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello World! acá va un UUID :D " + UUID.randomUUID().toString());
         }
         
-        
     }
+    
 }
